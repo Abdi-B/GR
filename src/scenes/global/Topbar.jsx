20b448b3528892747b3150597a852d6540a353f9
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-05-30 22:36:30 +0300

    first commit

---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,57 @@
+import React, { useContext } from "react";
+
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationModeOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsModeOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonModeOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+import { Box, IconButton, InputBase, useTheme } from "@mui/material";
+import { ColorModeContext, tokens } from "../../theme";
+
+// const styledBox = styled(Box)``;
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+  // console.log("colorMode : " + colorMode);
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* SEARCH BAR */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      {/* ICONS  */}
+      <Box>
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationModeOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsModeOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonModeOutlinedIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default Topbar;

0332c4c104b4c3892f9c7545b675d4b12af99f41
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-06-05 21:56:47 +0300

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import { Box, Typography, useTheme } from "@mui/material";
 import React from "react";
 import { tokens } from "../theme";
 
-
 const Header = ({ title, subtitle }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  
   return (
     <Box mb="30px">
       <Typography
